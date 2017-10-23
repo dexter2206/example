65ef5a52c05f049e541bd290aebb6be0c1581006
dexter <dexter@katkonjal>
2017-10-23 09:18:30 +0200

    Add initializer to Employee class

--- laboratorium.py
+++ laboratorium.py
@@ -1,3 +1,7 @@
 class Employee(object):
     """A class representing employee."""
-    pass
+    
+    def __init__(self, name, surname, salary):
+        self.name = name
+        self.surname = surname
+        self.salary = salary
